97003650b97b36c11a934df7de0f18c8b9fabc98
AriaFallah <aria@fallah.us>
2016-01-30 03:58:17 -0500

    :zap: webpack tweaks

--- chrome/webpack.config.js
+++ chrome/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  devtool: 'source-map',
   entry: [
     './src/index',
   ],
@@ -15,16 +14,12 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false,
       },
     }),
+    new webpack.optimize.DedupePlugin(),
   ],
   module: {
     loaders: [{
